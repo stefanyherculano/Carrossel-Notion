7aa90b75e8813a0347fd762575780ca51c5dcbfe
stefanyherculano <stefanyherculano25@gmail.com>
2025-01-30 17:53:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function moverCarrossel(direction) {
     currentIndex += direction;
 
     if (currentIndex >= totalImages) {
-        currentIndex = 0;
+        currentIndex = 0; // Volta para a primeira imagem
     } else if (currentIndex < 0) {
-        currentIndex = totalImages - 1;
+        currentIndex = totalImages - 1; // Vai para a última imagem
     }
 
-    const offset = -currentIndex * 1080;
-    container.style.transform = `translateX(${offset}px)`;
+    const offset = -currentIndex * 1080; // Calcula o deslocamento com base no índice atual
+    container.style.transform = `translateX(${offset}px)`; // Move o container
 }
